84420ea774e47c34969322414e861490f4f04249
Tugce Pınarbas <154636997+tugcepinarbas@users.noreply.github.com>
2026-01-30 19:09:30 +0300

    Chest code and file have been updated

---
+++ Assets/LuduArts_InteractionSystem_tugcepinarbas/Scripts/Runtime/Chest.cs
@@ -0,0 +1,35 @@
+
+using UnityEngine;
+using InteractionSystem.Runtime.Core; // IInteractable arayüzünü kullanmak için
+
+namespace InteractionSystem.Runtime.Interactions
+{
+    public class Chest : MonoBehaviour, IInteractable
+    {
+        [Header("Chest Settings")]
+        [SerializeField] private bool m_IsOpen = false;         // Kapý açýk mý kapalý mý?
+        [SerializeField] private float m_OpenAngle = -90f;       // Açýlýnca kaç derece dönsün?
+
+
+        // Oyuncu 'E'ye bastýðýnda burasý çalýþýr
+        public void OnInteract()
+        {
+            m_IsOpen = !m_IsOpen;          // Kapý açýksa kapat, kapalýysa aç
+
+
+            float targetAngle = m_IsOpen ? m_OpenAngle : 0f;                 // Kapýyý kendi ekseninde (Y ekseninde) döndür
+
+            transform.localRotation = Quaternion.Euler(0, targetAngle, 0);   //Kapýyý döndür (Quaternion kullanmak açý karýþýklýklarýný önler)
+
+            // Console'a durumu yazdýr (Hata takibi için çok önemli)
+            Debug.Log("Sistem: Kapý durumu deðiþti. Yeni durum Açýk mý?: " + m_IsOpen);
+        }
+
+        // Arayüzün (Interface) istediði diðer zorunlu kýsýmlar
+        public bool CanInteract() => true;
+        public string GetInteractionPrompt() => m_IsOpen ? "Kapat (E)" : "Aç (E)";
+        public void OnInteractionStart() { }
+        public void OnInteractionEnd() { }
+
+    }
+}
